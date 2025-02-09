d5e44c458e0162fbb0488f3ee0da4fe1f7e70c12
Дмитрий <dim.mamaeff@yandex.ru>
2025-02-09 20:42:45 +0300

    TshirtsAndSocks https://contest.yandex.ru/contest/66792/problems/B

--- src/main/java/algorithmTrainingYandex6_0/homeTask1/tshirtsAndSocks/TshirtsAndSocks.java
+++ src/main/java/algorithmTrainingYandex6_0/homeTask1/tshirtsAndSocks/TshirtsAndSocks.java
@@ -5,35 +5,56 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 
 public class TshirtsAndSocks {
     public static void tshirtsAndSocks(int a, int b, int c, int d) throws IOException {
+        int bd=2000000000,ac=2000000000;
+        int ab=2000000000,cd=2000000000;
 
-
-
-        if(a>b){
-            if(c>d){
-                System.out.println(b+1+" "+(d+1));
-            }else if(c<d){
-                if(a>d){
-                    System.out.println(b+1+" "+(d+1));
-                } else{
-                    System.out.println(a+1+" "+(c+1));
+        if ((a>0)&(c>0)){
+            bd=b+d+2;
+        }
+        if((b>0)&(d>0)){
+            ac=a+c+2;
+        }
+        if((a>0)&(b>0)){
+            ab=Integer.max(a,b)+2;
+        }
+        if((c>0)&(d>0)){
+            ab=Integer.max(c,d)+2;
+        }
+        if(bd<ac){
+            if(ab<cd) {
+                if(bd<ab){
+                    System.out.println((b+1)+" "+(d+1));
+                }else{
+                    System.out.println((Integer.max(a,b)+1)+" "+(1));
+                }
+            }else {
+                if(bd<cd){
+                    System.out.println((b+1)+" "+(d+1));
+                }else{
+                    System.out.println((Integer.max(c,d)+1)+" "+(1));
                 }
             }
         }else {
-            if(c>d){
-                if(b>c){
-                    System.out.println(a+1+" "+(c+1));
-                } else{
-                    System.out.println(b+1+" "+(d+1));
+            if (ab < cd) {
+                if (ac < ab) {
+                    System.out.println((a + 1) + " " + (c + 1));
+                } else {
+                    System.out.println((Integer.max(a, b) + 1) + " " + (1));
                 }
-            }else{
-                System.out.println(a+1+" "+(c+1));
+            } else {
+                if (ac < cd) {
+                    System.out.println((a + 1) + " " + (c + 1));
+                } else {
+                    System.out.println((Integer.max(c, d) + 1) + " " + (1));
+                }
             }
         }
-
 
 
     }
